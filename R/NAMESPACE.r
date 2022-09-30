9db26706a1f8fa9d56e6e90e4195eff06b1a87e9
Hugo  VARET <hugo.varet@pasteur.fr>
2022-09-30 10:13:18 +0200

    URL bug fix

--- R/NAMESPACE.r
+++ R/NAMESPACE.r
@@ -1,5 +1,12 @@
 #' @exportPattern ^[a-zA-Z]
 #' @useDynLib rfPred
 #' @import GenomicRanges
+#' @import Rsamtools
+#' @import IRanges
+#' @import data.table
+#' @import methods
+#' @import parallel
+#' @importFrom utils read.table write.csv2
+#' @importFrom GenomeInfoDb seqlevels seqlevels<-
 #' @exportMethod rfPred_scores
 NULL
